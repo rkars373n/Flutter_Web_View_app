4fbafb973af1f186c24712322183a96aae5476ae
rkars373n <oxygenkill2803@gmail.com>
2025-09-10 01:01:13 +0630

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/flutter_webview_fotmob/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.flutter_webview_fotmob
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fotmob WebView',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("https://www.fotmob.com"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Fotmob")),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
